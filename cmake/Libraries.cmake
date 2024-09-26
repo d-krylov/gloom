2f8be82043c710f36a838111ceadc319f109abf2
Denis Krylov <krylovdenism@gmail.com>
2024-09-26 11:21:55 +0400

    Imgui in cmake fetch content
    Signed-off-by: Denis Krylov <krylovdenism@gmail.com>

--- cmake/Libraries.cmake
+++ cmake/Libraries.cmake
@@ -2,6 +2,12 @@ include(FetchContent)
 
 FetchContent_Declare(
 	glm
-	GIT_REPOSITORY	https://github.com/g-truc/glm.git
-	GIT_TAG 	1.0.1
+	GIT_REPOSITORY https://github.com/g-truc/glm.git
+	GIT_TAG 1.0.1
+)
+
+FetchContent_Declare(
+	imgui
+	GIT_REPOSITORY https://github.com/ocornut/imgui.git
+	GIT_TAG v1.91.2
 )
